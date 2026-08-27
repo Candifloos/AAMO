c880329c0ea8f92751d497e0dac36fefdbace2af
JakobLN <202208269@post.au.dk>
2026-08-27 07:55:36 +0200

    mmm, monke

--- Week 1/I. Absorption rates.py
+++ Week 1/I. Absorption rates.py
@@ -6,6 +6,7 @@ c = 299792458 #m/s
 m_e = 510.998e3 #eV
 k_B = 8.617e-5 #eV/K
 
+
 B = (256/243/np.sqrt(2))**2 * 4*np.pi**2 * c**3 * hbar / (3 * m_e**2 * alpha)
 print("Problem 1:")
 print(f"{B = :.4f} m^3 eV^-1 s^-2")
@@ -20,3 +21,8 @@ print(f"{rho = :.4e} eV s m^-3")
 
 I = rho * c
 print(f"{I = :.4e} eV m^-2")
+
+print("\nProblem 3:")
+W_ba = 4/3 * (256/(243 * np.sqrt(2)))**2 * w_ba**3 * hbar**2 / m_e**2 / alpha
+print(f"Absorption transition rate \dot N_ba = W_ba")
+print(f"\t{W_ba = :.4e} s^-1")
