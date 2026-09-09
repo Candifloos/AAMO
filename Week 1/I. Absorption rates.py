4f569ab9e7ca584a112f766a09d7841ec8bd2c11
Candifloos <sst1892@gmail.com>
2026-09-09 10:33:41 +0200

    forgor take home & added rate calc in week 1

--- Week 1/I. Absorption rates.py
+++ Week 1/I. Absorption rates.py
@@ -9,16 +9,19 @@ eV_to_J = 1.602*10**(-19)
 e_0 = 8.85418782*10**(-12)#farad / m  - eller s^4*A^2/kg/m^3
 e = 1.60217663 * 10**(-19)  #coulomb
 a_0 = 5.29 * 10**(-11)
+w_ba = (E(2) - E(1))/hbar
 
 r = 256/243/np.sqrt(2) * a_0
 B = (256/243/np.sqrt(2))**2 * 4*np.pi**2 * c**3 * hbar / (3 * m_e**2 * alpha)
 print("Problem 1:")
 print(f"{B = :.4f} m^3 eV^-1 s^-2")
 print(f"B = {B/(eV_to_J) :.4e} m^3 J^-1 s^-2")
+W = 4*alpha /(3*c**2) * w_ba**3 * (256/243/np.sqrt(2) * a_0)**2
+print(f"{W = :.3e}")
+
 
 print("\nProblem 2:")
 T = 2000 #K
-w_ba = (E(2) - E(1))/hbar
 print(f"{w_ba = :.4e} s^-1")
 rho = hbar*w_ba**3/(np.pi**2*c**3) * 1/(np.exp(hbar*w_ba/(k_B * T)) - 1)
 print(f"{rho = :.4e} eV s m^-3")
