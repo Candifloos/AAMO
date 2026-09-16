6a8d23c73e6091f0a8417cbea06311c6fa46935d
JakobLN <202208269@post.au.dk>
2026-09-16 09:37:59 +0200

    removed garbage

--- Week 4/Problem 14.19.py
+++ Week 4/Problem 14.19.py
@@ -7,23 +7,14 @@ thf_i = lambda thf, thi: np.cos(thf) - np.cos(thi) + (thf - thi) * np.sin(thi)
 thf = lambda thi: sco.root(thf_i, thi + 1.3 * np.pi, args=thi)["x"][0]
 
 
-# E = lambda thi: 2 * (np.sin(thf(thi)) - np.sin(thi))**2
+E = lambda thi: 2 * (np.sin(thf(thi)) - np.sin(thi))**2
 
-# this = np.linspace(0,np.pi/2,100)
-# fig, ax = plt.subplots()
-
-# Es = [E(thi) for thi in this]
-# ax.plot(this, Es)
-# print(f"Max of function: {max(Es):.3f}")
-
-# plt.show()
-
-thi = -np.pi/2 - np.pi/2 #np.pi/4
-thfs = np.linspace(-np.pi,np.pi,100)
+this = np.linspace(0,np.pi/2,100)
 fig, ax = plt.subplots()
-ys = [thf_i(thf, thi) for thf in thfs]
-ax.plot(thfs, ys)
-ax.axhline(0, color="grey", linestyle="--")
-ax.axvline(thi, color="grey", linestyle="--")
+
+Es = [E(thi) for thi in this]
+ax.plot(this, Es)
+print(f"Max of function: {max(Es):.3f}")
+
 
 plt.show()
