da70cc1df08ea91c22d514adf30ee880df78914f
JakobLN <202208269@post.au.dk>
2026-09-16 10:39:26 +0200

    fixed pi labels

--- Week 4/Problem 14.19.py
+++ Week 4/Problem 14.19.py
@@ -2,18 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as sco
 
-plt.rc("axes", labelsize=30, titlesize=32)   # skriftstørrelse af xlabel, ylabel og title
-plt.rc("xtick", labelsize=26, top=True, direction="in")  # skriftstørrelse af ticks, vis også ticks øverst og vend ticks indad
-plt.rc("ytick", labelsize=26, right=True, direction="in") # samme som ovenstående
-plt.rc("legend", fontsize=26) # skriftstørrelse af figurers legends
+plt.rc("axes", labelsize=20, titlesize=22)   # skriftstørrelse af xlabel, ylabel og title
+plt.rc("xtick", labelsize=16, top=True, direction="in")  # skriftstørrelse af ticks, vis også ticks øverst og vend ticks indad
+plt.rc("ytick", labelsize=16, right=True, direction="in") # samme som ovenstående
+plt.rc("legend", fontsize=20) # skriftstørrelse af figurers legends
 plt.rcParams["font.size"] = "20"
-plt.rcParams["figure.figsize"] = (16,8)
+plt.rcParams["figure.figsize"] = (8,5)
 
 thf_i = lambda thf, thi: np.cos(thf) - np.cos(thi) + (thf - thi) * np.sin(thi)
 
 thf = lambda thi: sco.root(thf_i, thi + 1.3 * np.pi, args=thi)["x"][0]
-
-
 E = lambda thi: 2 * (np.sin(thf(thi)) - np.sin(thi))**2
 
 this = np.linspace(0,np.pi/2,1000)
@@ -31,7 +29,8 @@ thfs = [thf(thi) for thi in this]
 ax.plot(this, thfs)
 
 
-ax.set_yticks(ticks = np.arange(0,2*np.pi+0.1, np.pi/4), labels=[f"{i}π/4" for i in range(9)])
-ax.set_xticks(ticks = np.arange(0,np.pi/2+0.01, np.pi/4), labels=[f"{i}π/4" for i in range(3)])
+piLabels = ["0", "π/4", "π/2", "3π/4", "π", "5π/4", "3π/2", "7π/4", "2π"]
+ax.set_yticks(ticks = np.arange(0,2*np.pi+0.1, np.pi/4), labels=piLabels[:9])
+ax.set_xticks(ticks = np.arange(0,np.pi/2+0.01, np.pi/4), labels=piLabels[:3])
 ax.grid()   
 plt.show()
